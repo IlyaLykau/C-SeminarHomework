ab2d27a0769aecb15ae671c3d7bde8220833b9eb
IlyaLykau <viperliv@gmail.com>
2023-02-27 10:50:24 +0300

    add Seminar01 Homework 02

--- Seminar01_Homework/Homework001/Program.cs
+++ Seminar01_Homework/Homework001/Program.cs
@@ -10,6 +10,6 @@ Console.WriteLine("Введите второе число ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num1 < num2)
-Console.WriteLine("Большее число " + num2);
+  Console.WriteLine("Большее число " + num2);
 else 
-Console.WriteLine("Большее число " + num1);
+  Console.WriteLine("Большее число " + num1);
