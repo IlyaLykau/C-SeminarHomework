d7ad6115239dac0f42c20ad317044d7322a6e410
IlyaLykau <viperliv@gmail.com>
2023-02-28 11:32:10 +0300

    add Homework 02_02

--- Seminar02_Homework/Homework_02_02/Program.cs
+++ Seminar02_Homework/Homework_02_02/Program.cs
@@ -4,3 +4,18 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100)
+{
+  while (num > 999)
+  {
+    num = num / 10;
+  }
+  num = num % 10;
+Console.WriteLine("Третья цифра это: " + num);
+}
+else
+Console.WriteLine("Третьей цифры нет");
+
