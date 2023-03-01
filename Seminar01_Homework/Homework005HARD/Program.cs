ab6a87ead49f1f809f17a15e2b11c82b1487a861
IlyaLykau <viperliv@gmail.com>
2023-03-01 11:09:56 +0300

    change homework 005 HARD

--- Seminar01_Homework/Homework005HARD/Program.cs
+++ Seminar01_Homework/Homework005HARD/Program.cs
@@ -10,3 +10,14 @@
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
+if (num > 100)
+{
+  while (num > 999)
+  {
+    num = num / 10;
+  }
+    num = num % 10;
+Console.WriteLine("третья цифра это: " + num);
+} 
+else
+  Console.WriteLine("третьей цифры нет");
