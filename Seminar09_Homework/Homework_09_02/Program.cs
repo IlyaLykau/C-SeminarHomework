e303bd59a7eecf104d1f041ecd81dc81623d261a
IlyaLykau <viperliv@gmail.com>
2023-03-26 13:55:08 +0300

    add Homework Seminar 09

---
+++ Seminar09_Homework/Homework_09_02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("введите число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NumRec(N, M));
+Console.WriteLine($"сумма равна:{SummaRec(M, N)}");
+
+string NumRec(int N, int M)
+{
+  if (N == M) return Convert.ToString(M);
+  return NumRec(N - 1, M) + " " + Convert.ToString(N);
+}
+
+int SummaRec(int M, int N)
+{
+  if (M == N) return N;
+  return N + SummaRec(M, N - 1);
+}
+
